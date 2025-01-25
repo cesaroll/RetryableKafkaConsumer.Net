719673982a8d10ae3117e90f9d877a2b5d8ad0a4
cl352v_wayfair <clopezlerma@wayfair.com>
2025-01-25 19:44:08 +0100

    ProfOfConcept

--- harness/Harness/obj/Debug/net9.0/Harness.AssemblyInfo.cs
+++ harness/Harness/obj/Debug/net9.0/Harness.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Harness")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c44806b8e74e7e9a2e1ef82dfcdfab349ab9ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15d0ccf915887a92049951b2dc3c0257f86c66b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Harness")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Harness")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/RetryableKafkaConsumer.Contracts/obj/Debug/net9.0/RetryableKafkaConsumer.Contracts.AssemblyInfo.cs
+++ src/RetryableKafkaConsumer.Contracts/obj/Debug/net9.0/RetryableKafkaConsumer.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RetryableKafkaConsumer.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55c44806b8e74e7e9a2e1ef82dfcdfab349ab9ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15d0ccf915887a92049951b2dc3c0257f86c66b")]
 [assembly: System.Reflection.AssemblyProductAttribute("RetryableKafkaConsumer.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RetryableKafkaConsumer.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
